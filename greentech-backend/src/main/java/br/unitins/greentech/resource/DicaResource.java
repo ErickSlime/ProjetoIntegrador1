24dcc247a5c7e43f042567f8991e493461048589
Emannuel Oliveira Pedreira <emannuelop22@gmail.com>
2023-11-14 20:18:29 -0300

    Resource

---
+++ greentech-backend/src/main/java/br/unitins/greentech/resource/DicaResource.java
@@ -0,0 +1,7 @@
+package br.unitins.greentech.resource;
+
+import jakarta.inject.Inject;
+
+public class DicaResource {
+    
+}
---
+++ greentech-backend/src/main/java/br/unitins/greentech/resource/UsuarioResource.java
@@ -0,0 +1,5 @@
+package br.unitins.greentech.resource;
+
+public class UsuarioResource {
+    
+}
