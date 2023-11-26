8c3a3dd86517bbe29aad9d1aa18c9de0764172dd
Emannuel Oliveira Pedreira <emannuelop22@gmail.com>
2023-11-26 10:17:59 -0300

    Form do video
    O link dos videos não estar funcionando

--- greentech-backend/src/main/java/br/unitins/greentech/dto/VideoResponseDTO.java
+++ greentech-backend/src/main/java/br/unitins/greentech/dto/VideoResponseDTO.java
@@ -1,5 +1,19 @@
 package br.unitins.greentech.dto;
 
-public record VideoResponseDTO () {
+import br.unitins.greentech.model.Video;
+
+public record VideoResponseDTO (
+    Long id,
+    String nome,
+    String urlVideo,
+    String descricao
+) {
+    public static VideoResponseDTO valueOf(Video video) {
+            return new VideoResponseDTO(
+                    video.getId(),
+                    video.getNome(),
+                    video.getUrlVideo(),
+                    video.getDescricao());
+        }
     
 }
--- greentech-frontend/src/app/video/components/video-form/video-form.component.ts
+++ greentech-frontend/src/app/video/components/video-form/video-form.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Video } from 'src/app/models/video.model';
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
   selector: 'app-video-form',
   templateUrl: './video-form.component.html',
   styleUrls: ['./video-form.component.css']
 })
-export class VideoFormComponent {
+export class VideoFormComponent implements OnInit {
 
   formGroup: FormGroup;
   apiResponse: any = null;
@@ -18,15 +19,52 @@ export class VideoFormComponent {
   pageSize = 10;
   pagina = 0;
 
-constructor(private formBuilder: FormBuilder,
-  private activatedRoute: ActivatedRoute) {
+  ngOnInit(): void {
 
+    this.videoService.findAll().subscribe(data => {
+      this.videos = data;
+      this.initializeForm();
+    });
+    this.initializeForm();
+  }
+
+constructor(private formBuilder: FormBuilder,
+  private activatedRoute: ActivatedRoute,
+  private router: Router,
+  private videoService: VideoService) {
+
+    this.formGroup = this.formBuilder.group({
+      id:[null],
+      nome:[ '', Validators.required],
+      urlVideo:[ '', Validators.required],
+      descricao:[ '', Validators.required]
+    })
+}
+
+initializeForm(){
   const video: Video = this.activatedRoute.snapshot.data['video'];
-  this.formGroup = formBuilder.group({
+
+  this.formGroup = this.formBuilder.group({
     id: [(video && video.id) ? video.id : null],
     nome: [(video && video.nome) ? video.nome : '', Validators.required],
+    urlVideo: [(video && video.urlVideo) ? video.urlVideo : '', Validators.required],
     descricao: [(video && video.descricao) ? video.descricao : '', Validators.required]
   })
+
+}
+
+salvar() {
+  if (this.formGroup.valid) {
+    const video = this.formGroup.value;
+    console.log(video);
+    if (video.id == null) {
+      this.videoService.save(video).subscribe({
+        next: (videoCadastrada) => {
+          this.router.navigateByUrl('/videos/card-list');
+        }}
+        )
+      }
+      }
 }
 
 }
